5ae914a4fb06a8860431899e1111f7933184ca1e
muhammedjaseel <appcare@APPCAREs-MacBook-Air.local>
2023-03-25 17:31:12 +0530

    Ordinery Push

--- src/app/home-dashboard/c-store/c-store.component.ts
+++ src/app/home-dashboard/c-store/c-store.component.ts
@@ -10,17 +10,101 @@ export class CStoreComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  catogoryWaysSales = [
+    { title: 'Lubricants', value: 100 }, //value in persentage
+    { title: 'Grocery', value: 90 },
+    { title: 'Snacks', value: 80 },
+    { title: 'Beverages', value: 90 },
+    { title: 'Services', value: 60 },
+    { title: 'Vegetables', value: 40 },
+  ];
+  catogoryWaysStocks = [
+    {
+      title: 'Lubricants',
+      value: 100, //value in persentage
+      valueIn: 75, //value in persentage
+      color: '#F0A04A7B',
+      colorIn: '#F0A04A',
+    },
+    {
+      title: 'Grocery',
+      value: 90,
+      valueIn: 50,
+      color: '#9B1A527B',
+      colorIn: '#9B1A52',
+    },
+    {
+      title: 'Snacks',
+      value: 80,
+      valueIn: 40,
+      color: '#1A529B7B',
+      colorIn: '#1A529B',
+    },
+    {
+      title: 'Beverages',
+      value: 90,
+      valueIn: 50,
+      color: '#D073BA7B',
+      colorIn: '#D073BA',
+    },
+    {
+      title: 'Services',
+      value: 60,
+      valueIn: 50,
+      color: '#73CDD07B',
+      colorIn: '#73CDD0',
+    },
+    {
+      title: 'Vegetables',
+      value: 40,
+      valueIn: 30,
+      color: '#85D0737B',
+      colorIn: '#85D073',
+    },
+  ];
+
   mopWiseSale: any = [
-    { value: 20, title: 'CASH', color: '#809EED' },
+    { value: 20, title: 'CASH', color: '#809EED' }, //value in persentage
     { value: 65, title: 'CARD', color: '#26428b' },
     { value: 15, title: 'UPI', color: '#4a6dcb' },
+  ];
+
+  lowSlockItems = [
+    {
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg',
+      name: 'Aquafina water bottle 1 litre',
+      qty: '1 PSC',
+      textColor: '#C91717',
+      bgColor: '#F9D1D17B',
+    },
+    {
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg',
+      name: 'Lay s Potato Chips 52g',
+      qty: '1 PSC',
+      textColor: '#039003',
+      bgColor: '#0390037B',
+    },
+    {
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg',
+      name: 'Pepsi Soft Drink, 750ml Pet Bottle',
+      qty: '1 PSC',
+      textColor: '#C91717',
+      bgColor: '#F9D1D17B',
+    },
+    {
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg',
+      name: 'Pepsi Mountain Dew Soft Drink Can',
+      qty: '1 PSC',
+      textColor: '#C91717',
+      bgColor: '#F9D1D17B',
+    },
   ];
 
   // ////////////////////////////////////////////////////////////////////////
   // ////////////////////////////////////////////////////////////////////////
   // ////////////////////////////////////////////////////////////////////////
   // ////////////////////////////////////////////////////////////////////////
-  // TEMPLATE VARIABLES
+  // <--START--> TEMPLATE VARIABLES
   makeArryForPie = (array: any, k: number) => {
     const c = array[k]?.color || 'gray';
     var s = 0;
@@ -32,4 +116,5 @@ export class CStoreComponent implements OnInit {
     return element;
   };
   pieChardSelected = 0;
+  // <--END--> TEMPLATE VARIABLES
 }
