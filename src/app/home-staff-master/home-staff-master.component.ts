c92d84e443f6b333c71f8a95ac8af0df0d4055b1
Muhammed Jaseel M <muhammedjaseelm@Muhammeds-MacBook-Air.local>
2023-04-16 02:20:44 -0700

    Ordinery Push

--- src/app/home-staff-master/home-staff-master.component.ts
+++ src/app/home-staff-master/home-staff-master.component.ts
@@ -14,7 +14,7 @@ export class HomeStaffMasterComponent implements OnInit {
   allPayrun: any = allDummyEmployee;
   allPayrunEmploye: any = allDummyPayrunEmployee;
 
-  page: string = 'staffMaster';
+  page: string = 'addPayRun';
   popUp: String = '';
   isSystemUser: boolean = false;
 
